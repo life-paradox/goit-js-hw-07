843dbbfd00d603d0d0a015c2f11e678d5da84f6b
life-paradox <zhideevpavel@gmail.com>
2022-08-28 23:08:57 +0300

    Solutions of tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const items = galleryItems.map(option => {
+  const galleryItem = document.createElement(`div`);
+  galleryItem.classList.add('gallery__item');
+  galleryItem.insertAdjacentHTML(
+    'beforeend',
+    `<a class="gallery__link" href="${option.original}">
+    <img
+      class="gallery__image"
+      src="${option.preview}"
+      data-source="${option.original}"
+      alt="${option.description}"
+    />
+  </a>`
+  );
+  return galleryItem;
+});
+
+gallery.append(...items);
+
+gallery.addEventListener('click', onItemGallery);
+
+function onItemGallery(event) {
+  event.preventDefault();
+
+  const modal = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="1280" >
+`);
+  modal.show();
+
+  window.addEventListener('keydown', onEscape);
+  function onEscape(event) {
+    if (event.keyCode === 27) {
+      modal.close();
+      window.removeEventListener('keydown', onEscape);
+    }
+  }
+}
 
 console.log(galleryItems);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+
+const items = galleryItems.map(option => {
+  const galleryItem = galleryEl.insertAdjacentHTML(
+    'beforeend',
+    `<a class="gallery__item" href="${option.original}">
+      <img
+        class="gallery__image"
+        src="${option.preview}"
+        alt="${option.description}"
+      />
+    </a>`
+  );
+  return galleryItem;
+});
+
+var lightbox = new SimpleLightbox('.gallery a', { captions: false });
 
 console.log(galleryItems);
